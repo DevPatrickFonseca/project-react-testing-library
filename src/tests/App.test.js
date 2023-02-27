c40cb5510ac290145b62dbe98ddee44ae7a5e60c
PFonsecaFV <pfonsecafv@gmail.com>
2023-02-27 15:36:19 -0300

    Requisito 01 - Concluído

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 01 - Teste o componente <App.js />', () => {
+  test('1 - Se primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  test('2 - Se primeiro link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  test('3 - Se primeiro link deve possuir o texto Favorite Pokémon', () => {
+    renderWithRouter(<App />);
+    const linkFavoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+
+    expect(linkFavoritePokemon).toBeInTheDocument();
+  });
+});
