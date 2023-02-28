7f9fc0f40b93a5c713c42bb507e03f2efd3e304e
PFonsecaFV <pfonsecafv@gmail.com>
2023-02-28 10:59:04 -0300

    Requisito 03 - Concluído

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Requisito 03 - Teste o componente <FavoritePokemon.js />', () => {
+  test('1 - Se favoritar a partir da página de detalhes é exibida na tela mensagem "No favorite Pokémon found"', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const messageNoFound = screen.getByText(/No favorite Pokémon found/i);
+
+    expect(messageNoFound).toBeInTheDocument();
+  });
+
+  test('2 - Se favoritar a partir da página de detalhes apenas são exibidos os Pokémon favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    // Pokémon - More details
+    const pokemonMoreDetailsSendToFavorited = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(pokemonMoreDetailsSendToFavorited);
+
+    // Pokémon - Favorited Pokémon
+    const checkboxFavoritePokemon = screen.getByRole('checkbox', { name: /Pokémon favoritado/i });
+    userEvent.click(checkboxFavoritePokemon);
+
+    // Pokémon - Favorite Pokémon History
+    const favoritePokemon = screen.getByRole('link', { name: /Favorite Pokémon/i });
+    userEvent.click(favoritePokemon);
+    expect(history.location.pathname).toBe('/favorites');
+
+    //  Pokémon - Favorite page has a Pokémon Image and Star Image
+    const hasPokemonImage = screen.getByRole('img', { name: /sprite/i });
+    expect(hasPokemonImage).toBeInTheDocument();
+
+    const hasPokemonStarImage = screen.getByRole('img', { name: /is marked as favorite/i });
+    expect(hasPokemonStarImage).toBeInTheDocument();
+  });
+});
